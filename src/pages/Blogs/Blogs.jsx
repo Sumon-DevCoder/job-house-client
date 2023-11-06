3607b25888bd80558e457eb5734314d8ce4c4ec4
SumonRahman24 <mustafizurrahmansumon24@gmail.com>
2023-11-06 17:52:46 +0600

    applied job page  added search filtering by category

--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -7,7 +7,7 @@ const Blogs = () => {
         <title>Blogs</title>
       </Helmet>
 
-      <section className="bg-white dark:bg-gray-900">
+      <section className="bg-white dark:bg-gray-900 mt-10">
         <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
           <div className="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
             <h2 className="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
@@ -20,28 +20,24 @@ const Blogs = () => {
           </div>
           <div className="grid gap-8 lg:grid-cols-2">
             <article className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-              <div className="flex justify-between items-center mb-5 text-gray-500">
-                <span className="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
-                  <svg
-                    className="mr-1 w-3 h-3"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path>
-                  </svg>
-                  Tutorial
-                </span>
-                <span className="text-sm">14 days ago</span>
-              </div>
+              <div className="flex justify-between items-center  text-gray-500"></div>
               <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                <a href="#">How to quickly deploy a static website</a>
+                <a href="#">
+                  What is an access token and refresh token? How do they work
+                  and where should we store them on the client-side?
+                </a>
               </h2>
-              <p className="mb-5 font-light text-gray-500 dark:text-gray-400">
-                Static websites are now used to bootstrap lots of websites and
-                are becoming the basis for a variety of tools that even
-                influence both web designers and developers influence both web
-                designers and developers.
+              <p className="mb-5 font-medium text-gray-500 dark:text-gray-400">
+                access token has expire date or time and refresh token work to
+                regenerate accessToken that means when access token will be
+                expired that time refresh token create new token for user.
+                <br />
+                access token and refresh both of make verify user that means
+                when user login then from backend give a access token to user.
+                and user when access her/she data to show that time will verify
+                that user is verified by access token. if user invalid then
+                server apis do not give data and if user valid then server give
+                me data.
               </p>
               <div className="flex justify-between items-center">
                 <div className="flex items-center space-x-4">
@@ -73,33 +69,18 @@ const Blogs = () => {
               </div>
             </article>
             <article className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-              <div className="flex justify-between items-center mb-5 text-gray-500">
-                <span className="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
-                  <svg
-                    className="mr-1 w-3 h-3"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z"
-                      clipRule="evenodd"
-                    ></path>
-                    <path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"></path>
-                  </svg>
-                  Article
-                </span>
-                <span className="text-sm">14 days ago</span>
-              </div>
+              <div className="flex justify-between items-center  text-gray-500"></div>
               <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                <a href="#">Our first project with React</a>
+                <a href="#"> What is express js? What is Nest JS ?</a>
               </h2>
-              <p className="mb-5 font-light text-gray-500 dark:text-gray-400">
-                Static websites are now used to bootstrap lots of websites and
-                are becoming the basis for a variety of tools that even
-                influence both web designers and developers influence both web
-                designers and developers.
+              <p className="mb-5  font-medium text-gray-500 dark:text-gray-400">
+                express js is a frameworks of node Js and it will be easy,
+                simple, and quiker. we can create server easily and it helpful
+                for maintance. <br />
+                Next Js is a popular javascript library and it is often used in
+                combination with React it has wide range of featues. not only
+                that it has server side rendering which help improve to seo
+                friendly and fast loading page features.
               </p>
               <div className="flex justify-between items-center">
                 <div className="flex items-center space-x-4">
